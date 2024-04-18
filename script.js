4247e3d9678ffa5aa411998aca5b8c1de7247dfa
vorvixy <147879949+vorvixy@users.noreply.github.com>
2024-04-18 22:27:58 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+
+yesBtn.addEventListener("click", () => {
+    question.innerHTML = "Я так и знала...";
+    gif.src =
+        "https://i.pinimg.com/originals/da/6a/af/da6aaf453791ff979b564fdfe9a0a166.gif";
+});
+
+noBtn.addEventListener("mouseover", () => {
+    const noBtnRect = noBtn.getBoundingClientRect();
+    const maxX = window.innerWidth - noBtnRect.width;
+    const maxY = window.innerHeight - noBtnRect.height;
+
+    const randomX = Math.floor(Math.random() * maxX);
+    const randomY = Math.floor(Math.random() * maxY);
+
+    noBtn.style.left = randomX + "px";
+    noBtn.style.top = randomY + "px";
+});
